a3e6cf74de367be04f851fd4802633b58e9f7663
Paris Kasidiaris <paris@sourcelair.com>
2019-01-06 20:28:37 +0200

    v0.2.1 - Fix prose static js

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django_prose",
-    version="0.2.0",
+    version="0.2.1",
     description=(
         "Elegant prose authoring for Django"
     ),
