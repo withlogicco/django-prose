efa4c0e18e18cea5c576fe4c3d67d3c82b173772
Paris Kasidiaris <paris@sourcelair.com>
2022-04-12 17:51:26 +0300

    Format

--- prose/fields.py
+++ prose/fields.py
@@ -6,14 +6,41 @@ from prose.widgets import RichTextEditor
 
 
 ALLOWED_TAGS = [
-    "p", "ul", "ol", "li", "strong", "em", "div", "span", "a",
-    "blockquote", "pre", "figure", "figcaption", "br", "code",
-    "h1", "h2", "h3", "h4", "h5", "h6",
-    "picture", "source", "img",
+    "p",
+    "ul",
+    "ol",
+    "li",
+    "strong",
+    "em",
+    "div",
+    "span",
+    "a",
+    "blockquote",
+    "pre",
+    "figure",
+    "figcaption",
+    "br",
+    "code",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "picture",
+    "source",
+    "img",
 ]
 ALLOWED_ATTRIBUTES = [
-    "alt", "class", "id", "src", "srcset", "href", "media",
+    "alt",
+    "class",
+    "id",
+    "src",
+    "srcset",
+    "href",
+    "media",
 ]
+
 
 class RichTextField(models.TextField):
     def formfield(self, **kwargs):
@@ -22,7 +49,9 @@ class RichTextField(models.TextField):
 
     def pre_save(self, model_instance, add):
         raw_html = getattr(model_instance, self.attname)
-        sanitized_html = bleach.clean(raw_html, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES)
+        sanitized_html = bleach.clean(
+            raw_html, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES
+        )
         return sanitized_html
 
 
