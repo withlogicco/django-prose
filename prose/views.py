9bba65e22d5bb70dd52c82bb71b5937572f24af5
Paris Kasidiaris <paris@kasidiaris.com>
2023-06-07 11:33:52 +0300

    Format

--- prose/views.py
+++ prose/views.py
@@ -18,7 +18,10 @@ def upload_attachment(request):
     if request.method == "POST":
         attachment = request.FILES["file"]
         if not validate_file(attachment):
-            return JsonResponse({"error": f"Files must be {ALLOWED_FILE_SIZE}MB or smaller."}, status=400)
+            return JsonResponse(
+                {"error": f"Files must be {ALLOWED_FILE_SIZE}MB or smaller."},
+                status=400,
+            )
         key = f"{datetime.now().strftime('%Y/%m/%d')}/{uuid4()}.{attachment.name.split('.')[-1]}"
         path = f"prose/{key}"
         default_storage.save(path, attachment)
