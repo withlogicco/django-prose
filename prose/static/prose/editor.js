db59b67008dc80bfb2b91e8444f44eeaaf960cff
Onno T <onno@code-on.be>
2023-07-05 12:41:09 +0200

    editor.js has initialize command. So with htmx or other ajax you can call the script

--- prose/static/prose/editor.js
+++ prose/static/prose/editor.js
@@ -1,70 +1,75 @@
-// Handle uploads
-
+// This code goes in /static/prose/editor.js or similar
 function uploadAttachment(host, attachment) {
-  uploadFile(host, attachment, setProgress, setAttributes);
+    uploadFile(host, attachment.file, setProgress, setAttributes);
 
-  function setProgress(progress) {
-    attachment.setUploadProgress(progress);
-  }
-
-  function setAttributes(attributes) {
-    attachment.setAttributes(attributes);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  const editors = document.querySelectorAll('.django-prose-editor');
-
-  editors.forEach(editor => {
-    addEventListener("trix-attachment-add", function(event) {
-      uploadAttachment(editor.dataset.uploadAttachmentUrl, event.attachment);
-    });
-  });
-});
-
-function uploadFile(host, attachment, progressCallback, successCallback) {
-  var formData = createFormData(attachment.file);
-  var xhr = new XMLHttpRequest();
-
-  const csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value;
-
-  formData.append('csrfmiddlewaretoken', csrfToken);
-
-  xhr.open("POST", host, true);
-
-  xhr.upload.addEventListener("progress", function(event) {
-    var progress = event.loaded / event.total * 100;
-    progressCallback(progress);
-  });
-
-  xhr.addEventListener("load", function(event) {
-    let data = {};
-    switch (xhr.status) {
-      case 201:
-        data = JSON.parse(xhr.response);
-        var attributes = {
-          url: data.url,
-          href: `${data.url}?content-disposition=attachment`
-        };
-        successCallback(attributes);
-        break;
-      case 400:
-        data = JSON.parse(xhr.response);
-        alert(data.error);
-        attachment.remove();
-        break;
-      default:
-        alert("The upload failed.");
-        attachment.remove();
+    function setProgress(progress) {
+        attachment.setUploadProgress(progress);
     }
-  });
 
-  xhr.send(formData);
+    function setAttributes(attributes) {
+        attachment.setAttributes(attributes);
+    }
 }
 
-function createFormData(file) {
-  var data = new FormData();
-  data.append("file", file);
-  data.append("Content-Type", file.type);
-  return data;
+function uploadFile(host, file, progressCallback, successCallback) {
+    var key = createStorageKey(file);
+    var formData = createFormData(key, file);
+    var xhr = new XMLHttpRequest();
+
+    const csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value;
+
+    formData.append('csrfmiddlewaretoken', csrfToken);
+
+    xhr.open("POST", host, true);
+
+    xhr.upload.addEventListener("progress", function(event) {
+        var progress = event.loaded / event.total * 100;
+        progressCallback(progress);
+    });
+
+    xhr.addEventListener("load", function(event) {
+        if (xhr.status == 201) {
+            const data = JSON.parse(xhr.response);
+
+            var attributes = {
+                url: data.url,
+                href: `${data.url}?content-disposition=attachment`
+            };
+            successCallback(attributes);
+        }
+    });
+
+    xhr.send(formData);
 }
+
+function createStorageKey(file) {
+    var date = new Date();
+    var day = date.toISOString().slice(0,10);
+    var name = date.getTime() + "-" + file.name;
+    return [day, name].join("/");
+}
+
+function createFormData(key, file) {
+    var data = new FormData();
+    data.append("key", key);
+    data.append("file", file);
+    data.append("Content-Type", file.type);
+    return data;
+}
+
+function initializeEditors() {
+    const editors = document.querySelectorAll('.django-prose-editor:not(.initialized)');
+
+    editors.forEach(editor => {
+        editor.addEventListener("trix-attachment-add", function(event) {
+            uploadAttachment(editor.dataset.uploadAttachmentUrl, event.attachment);
+        });
+        editor.classList.add('initialized');
+    });
+}
+
+// When the DOM is initially loaded
+document.addEventListener("DOMContentLoaded", initializeEditors);
+
+// Export the initializeEditors function so it can be called from other scripts
+window.initializeEditors = initializeEditors;
